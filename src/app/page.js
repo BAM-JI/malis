c905d72b5b1d635deb717dec58278d6dc22ef4a7
= <=gamner59@gmail.com>
2025-01-20 00:57:04 -0600

    se borraron algunas carpetas, tambien se modifico la estructura del proyecto

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,56 @@
-"use client"; // Agrega esta línea al inicio del archivo
-
+'use client'
 import { useState } from 'react';
+import { buscarPeliculas } from '../lib/buscar';
 
-export default function SearchPage() {
-  const [query, setQuery] = useState('');
-  const [results, setResults] = useState([]);
+const Inicio = () => {
+  const [consulta, setConsulta] = useState('');
+  const [resultados, setResultados] = useState([]);
+  const [cargando, setCargando] = useState(false);
 
-  const handleSearch = async () => {
-    const res = await fetch(`/api/search?query=${query}`);
-    const data = await res.json();
-    setResults(data.hits || []);
+  const manejarBusqueda = async (e) => {
+    e.preventDefault();
+    if (!consulta) return;
+
+    setCargando(true);
+    const resultadosBusqueda = await buscarPeliculas(consulta);
+    setResultados(resultadosBusqueda);
+    setCargando(false);
   };
 
   return (
     <div>
-      <h1>Buscador</h1>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Escribe algo..."
-      />
-      <button onClick={handleSearch}>Buscar</button>
-      <ul>
-        {results.map((result) => (
-          <li key={result.id}>{result.nombre}</li>
-        ))}
-      </ul>
+      <h1>Buscador de Películas</h1>
+      <form onSubmit={manejarBusqueda}>
+        <input
+          type="text"
+          placeholder="Buscar película..."
+          value={consulta}
+          onChange={(e) => setConsulta(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      {cargando ? (
+        <p>Cargando...</p>
+      ) : (
+        <div>
+          <h2>Resultados de la búsqueda:</h2>
+          <ul>
+            {resultados.length === 0 ? (
+              <p>No se encontraron resultados.</p>
+            ) : (
+              resultados.map((pelicula, indice) => (
+                <li key={indice}>
+                  <h3>{pelicula.title}</h3>
+                  <p>{pelicula.description}</p>
+                </li>
+              ))
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default Inicio;
